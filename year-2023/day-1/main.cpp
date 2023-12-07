d066bbe48abc2193495177abf510f567e853b679
Sergii Lovygin <lovygin.dev@gmail.com>
2023-12-07 23:09:38 +0200

    [2023][day-1][trebuchet] Update a solution for the first half of the puzzle
    The new implementation is less effective but more readable,
    and it's a transition state for the second half of the puzzle.

--- year-2023/day-1/main.cpp
+++ year-2023/day-1/main.cpp
@@ -1,21 +1,35 @@
 #include <algorithm>
 #include <fstream>
-#include <sstream>
 #include <iostream>
 #include <iterator>
+#include <memory>
+#include <numeric>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <numeric>
-#include <memory>
 
+
+std::vector<std::uint16_t> parse_digits(const std::string& message) {
+    static constexpr auto DIGITS = R"(0123456789)";
+
+    auto digit_pos = message.find_first_of(DIGITS);
+    if (digit_pos == std::string::npos) {
+        return {};
+    }
+
+    std::vector<std::uint16_t> digits;
+    while (digit_pos != std::string::npos) {
+        digits.emplace_back(message[digit_pos] - '0');
+        digit_pos = message.find_first_of(DIGITS, digit_pos + 1);
+    }
+    return digits;
+}
 
 std::istringstream get_test_data() {
-    return std::istringstream(
-        "1abc2\n"
-        "pqr3stu8vwx\n"
-        "a1b2c3d4e5f\n"
-        "treb7uchet\n"
-    );
+    return std::istringstream("1abc2\n"
+                              "pqr3stu8vwx\n"
+                              "a1b2c3d4e5f\n"
+                              "treb7uchet\n");
 }
 
 std::unique_ptr<std::istream> get_document(const char* filename) {
@@ -26,24 +40,27 @@ std::unique_ptr<std::istream> get_document(const char* filename) {
     return std::make_unique<std::ifstream>(filename);
 }
 
+std::vector<std::uint16_t> get_calibrations(std::istream& in) {
+    std::vector<std::uint16_t> calibrations;
+    std::transform(
+        std::istream_iterator<std::string>(in), std::istream_iterator<std::string>(), std::back_inserter(calibrations),
+        [](const std::string& line) {
+            const auto digits = parse_digits(line);
+            if (digits.empty()) {
+                return 0;
+            }
+
+            return digits.front() * 10 + digits.back();
+        }
+    );
+    return calibrations;
+}
+
 int main(int argc, const char* argv[]) {
     const auto* filename = (argc == 2) ? argv[1] : nullptr;
-    auto document = get_document(filename);
-    std::vector<std::uint16_t> calibrations;
-        std::transform(
-            std::istream_iterator<std::string>(*document),
-            std::istream_iterator<std::string>(),
-            std::back_inserter(calibrations),
-            [](const std::string& line) {
-                static constexpr auto digits = R"(0123456789)";
-                const auto first = line.find_first_of(digits);
-                const auto second = line.find_last_of(digits);
+    auto        document = get_document(filename);
 
-                const auto number = line.substr(first, 1) + line.substr(second, 1);
-                return std::stoul(number);
-            }
-        );
-
+    const std::vector<std::uint16_t> calibrations = get_calibrations(*document);
     std::cout << "calibration data (" << calibrations.size() << " items): ";
     std::copy(calibrations.cbegin(), calibrations.cend(), std::ostream_iterator<std::uint16_t>(std::cout, " "));
     std::cout << std::endl;
