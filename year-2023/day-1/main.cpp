158f51baeab35a0e79a2980efc59550dcb6f194a
Sergii Lovygin <lovygin.dev@gmail.com>
2023-12-09 02:56:30 +0200

    [2023][day-1][trebuchet] Update a solution for the first half of the puzzle
    It gives correct answer, but still be optimized!

--- year-2023/day-1/main.cpp
+++ year-2023/day-1/main.cpp
@@ -4,34 +4,29 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <map>
 #include <numeric>
+#include <set>
 #include <stdexcept>
 #include <string>
 #include <string_view>
+#include <utility>
 #include <vector>
 
 
 namespace parsing {
     constexpr std::string_view DIGITS = R"(0123456789)";
 
-    constexpr std::array<std::string_view, 10> DIGITS_WORDS{"zero", "one", "two",   "three", "four",
-                                                            "five", "six", "seven", "eight", "nine"};
-
     constexpr auto is_single_digit(char symbol) -> bool {
         return std::find(DIGITS.cbegin(), DIGITS.cend(), symbol) != DIGITS.cend();
     };
 
-    auto digit_start(char symbol) -> bool {
-        const auto digit_it = std::find(DIGITS.cbegin(), DIGITS.cend(), symbol);
-        if (digit_it != DIGITS.cend()) {
-            return true;
-        }
+    const std::map<std::string_view, std::uint16_t> DIGITS_MAP{
+        {"0", 0},    {"1", 1},   {"2", 2},     {"3", 3},     {"4", 4},
+        {"5", 5},    {"6", 6},   {"7", 7},     {"8", 8},     {"9", 9},
 
-        const auto word_it = std::find_if(DIGITS_WORDS.cbegin(), DIGITS_WORDS.cend(), [symbol](const auto& word) {
-            return word.front() == symbol;
-        });
-
-        return word_it != DIGITS_WORDS.cend();
+        {"zero", 0}, {"one", 1}, {"two", 2},   {"three", 3}, {"four", 4},
+        {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9},
     };
 
     template<typename Prefix>
@@ -39,10 +34,11 @@ namespace parsing {
               || std::is_same_v<Prefix, std::string_view>
     std::vector<std::string_view> get_partial_matches(Prefix prefix) {
         std::vector<std::string_view> candidates;
-        std::copy_if(
-            DIGITS_WORDS.cbegin(), DIGITS_WORDS.cend(), std::back_inserter(candidates),
-            [prefix](const std::string_view& word) { return word.starts_with(prefix); }
-        );
+        for (const auto& [word, _] : DIGITS_MAP) {
+            if (word.starts_with(prefix)) {
+                candidates.emplace_back(word);
+            }
+        }
         return candidates;
     }
 
@@ -54,45 +50,65 @@ namespace parsing {
     }
 
     std::uint16_t to_digit(std::string_view word) {
-        const auto it = std::find(DIGITS_WORDS.cbegin(), DIGITS_WORDS.cend(), word);
-        if (it == DIGITS_WORDS.cend()) {
+        const auto it = DIGITS_MAP.find(word);
+        if (it == DIGITS_MAP.cend()) {
             throw std::invalid_argument("unknown digit spelling");
         }
 
-        return std::distance(DIGITS_WORDS.cbegin(), it);
+        return it->second;
     }
 
-    std::vector<std::uint16_t> parse_digits(std::string_view string) {
-        std::vector<std::uint16_t> digits;
+    std::uint16_t get_first_digit(std::string_view line) {
+        const auto digit_start = [](char symbol) -> bool {
+            static const std::set<char> Filter{
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'z', 'o', 't', 'f', 's', 'e', 'n',
+            };
+            return Filter.contains(symbol);
+        };
 
-        auto cursor = std::find_if(string.cbegin(), string.cend(), digit_start);
-        while (cursor != string.cend()) {
+        auto cursor = std::find_if(line.cbegin(), line.cend(), digit_start);
+        while (cursor != line.cend()) {
             if (is_single_digit(*cursor)) {
-                digits.emplace_back(to_digit(*cursor));
-                cursor++;
-            } else {
-                const auto candidates = get_partial_matches(*cursor);
-                const auto it = std::find_if(candidates.cbegin(), candidates.cend(), [&](const std::string_view& word) {
-                    if (word.size() > std::distance(cursor, string.cend())) {
-                        return false;
-                    }
-
-                    const auto end = cursor + static_cast<std::string_view::difference_type>(word.size());
-                    return std::search(cursor, end, word.cbegin(), word.cend()) != end;
-                });
-
-                if (it != candidates.cend()) {
-                    digits.emplace_back(to_digit(*it));
-                    std::advance(cursor, it->size());
-                } else {
-                    cursor++;
-                }
+                return to_digit(*cursor);
             }
 
-            cursor = std::find_if(cursor, string.cend(), digit_start);
+            const auto candidates = get_partial_matches(*cursor);
+            const auto it         = std::find_if(candidates.cbegin(), candidates.cend(), [&](const std::string_view& word) {
+                if (word.size() > std::distance(cursor, line.cend())) {
+                    return false;
+                }
+
+                const auto end = cursor + static_cast<std::string_view::difference_type>(word.size());
+                return std::search(cursor, end, word.cbegin(), word.cend()) != end;
+            });
+
+            if (it != candidates.cend()) {
+                return to_digit(*it);
+            }
+
+            cursor = std::find_if(cursor + 1, line.cend(), digit_start);
         }
 
-        return digits;
+        return 0;
+    }
+
+    std::uint16_t get_last_digit(std::string_view line) {
+        std::vector<std::pair<std::string_view, std::string_view::difference_type>> results;
+        for (const auto& [word, _] : DIGITS_MAP) {
+            const auto it = std::find_end(line.cbegin(), line.cend(), word.cbegin(), word.cend());
+            if (it != line.cend()) {
+                results.emplace_back(word, std::distance(line.cbegin(), it));
+            }
+        }
+
+        if (results.empty()) {
+            return 0;
+        }
+
+        const auto it = std::max_element(results.cbegin(), results.cend(), [](auto&& lhs, auto&& rhs) {
+            return lhs.second < rhs.second;
+        });
+        return to_digit(it->first);
     }
 }  // namespace parsing
 
@@ -101,16 +117,12 @@ std::vector<std::uint16_t> get_calibrations(std::istream& in) {
 
     std::string line;
     while (std::getline(in, line)) {
-        const auto digits = parsing::parse_digits(line);
-        std::cout << line << " -> ";
-        std::copy(digits.cbegin(), digits.cend(), std::ostream_iterator<std::uint16_t>(std::cout, ""));
-
-        const auto first_digit = digits.empty() ? 0 : digits.front();
-        const auto last_digit  = digits.empty() ? 0 : digits.back();
+        const auto first_digit = parsing::get_first_digit(line);
+        const auto last_digit  = parsing::get_last_digit(line);
         const auto number      = first_digit * 10 + last_digit;  // NOLINT: concatenate two single digits into one number
-        std::cout << " -> " << number << std::endl;
-
         calibrations.emplace_back(number);
+
+        std::cout << line << " -> " << number << std::endl;
     }
     return calibrations;
 }
@@ -118,9 +130,9 @@ std::vector<std::uint16_t> get_calibrations(std::istream& in) {
 int main() {
     std::ifstream document(R"(input.txt)");
 
-    const std::vector<std::uint16_t> calibrations = get_calibrations(document);
-    const auto result = std::accumulate(calibrations.begin(), calibrations.end(), std::uint32_t{0});
-    std::cout << "result value is " << result << std::endl;
+    const auto calibrations = get_calibrations(document);
+    const auto result       = std::accumulate(calibrations.begin(), calibrations.end(), std::uint32_t{0});
+    std::cout << "The result value is " << result << std::endl;
 
     return 0;
 }
