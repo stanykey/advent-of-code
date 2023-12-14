fb62c8ca4857cc8cf3d8a32f59d175f2735c1cf5
Sergii Lovygin <lovygin.dev@gmail.com>
2023-12-15 01:08:13 +0200

    [2023][day-8][haunted-wasteland] Add description, inputs and stubs

---
+++ year-2023/day-8/main.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
+
+
+namespace io {
+    template<typename T>
+    T read(std::istream& input) {
+        T value{};
+        input >> value;
+        return value;
+    }
+
+    template<typename T>
+    std::vector<T> read(std::istream& input, std::size_t count) {
+        std::vector<T> result;
+
+        result.reserve(count);
+        while (count--) {
+            auto value = read<T>(input);
+            result.emplace_back(std::move(value));
+        }
+
+        return result;
+    }
+}  // namespace io
+
+
+int main() {
+    std::ifstream document(R"(input.txt)");
+
+    return 0;
+}
