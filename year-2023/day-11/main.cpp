e140265ad9faa3cd71e7e3339fe70f41cf9af113
Sergii Lovygin <lovygin.dev@gmail.com>
2023-12-21 11:51:59 +0200

    [project][2023] Add initial stubs for days 9-21

---
+++ year-2023/day-11/main.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <regex>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace io {
+    template<typename T>
+    T read(std::istream& input) {
+        T value{};
+        input >> value;
+        return value;
+    }
+
+    template<typename T>
+    std::vector<T> read(std::istream& input, std::size_t count) {
+        std::vector<T> result;
+
+        result.reserve(count);
+        while (count--) {
+            auto value = read<T>(input);
+            result.emplace_back(std::move(value));
+        }
+
+        return result;
+    }
+}  // namespace io
+
+int main() {
+    std::ifstream document(R"(test-data.txt)");
+    std::cout << "The result is " << 0 << std::endl;
+    return 0;
+}
